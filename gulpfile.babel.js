0453b747950810b161d44ab9842ba68703620764
Willmer Barahona <wbarahona@live.com>
2016-11-11 14:54:37 -0600

    cleaning hard tabs to soft
    cleaning hard tabs to soft

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,83 +2,83 @@
 // Gulpfile.js
 // Recipe by Willmer Barahona
 // -----------------------------------------------------------------------
-	// task related
-	import gulp from 'gulp';
-	import rename from 'gulp-rename';
-	import notify from 'gulp-notify';
-	import { argv } from 'yargs';
-	import prettyTime from 'pretty-hrtime';
+    // task related
+    import gulp from 'gulp';
+    import rename from 'gulp-rename';
+    import notify from 'gulp-notify';
+    import { argv } from 'yargs';
+    import prettyTime from 'pretty-hrtime';
 
-	// front end server related
-	import connect from 'gulp-connect';
+    // front end server related
+    import connect from 'gulp-connect';
 
-	// view/html/templating related
-	import handlebars from 'gulp-compile-handlebars';
+    // view/html/templating related
+    import handlebars from 'gulp-compile-handlebars';
 
-	// application configurations
-	import paths from './app/client/conf/paths.conf';
-	import web from './app/client/conf/web.conf';
-	import helpers from './app/client/conf/helpers.conf';
+    // application configurations
+    import paths from './app/client/conf/paths.conf';
+    import web from './app/client/conf/web.conf';
+    import helpers from './app/client/conf/helpers.conf';
 
 //
 // Internals
 // -----------------------------------------------------------------------
-	const internals 		= {
-								web: web,
-								paths: paths,
-								hbs: {
-									helpers: helpers
-								}
-							};
-	internals.hbs.options 	= {
-								ignorePartials: true,
-								batch : [internals.paths.dev.hbs.partials],
-								helpers : internals.hbs.helpers
-							};
+    const internals         = {
+                                web: web,
+                                paths: paths,
+                                hbs: {
+                                    helpers: helpers
+                                }
+                            };
+    internals.hbs.options   = {
+                                ignorePartials: true,
+                                batch : [internals.paths.dev.hbs.partials],
+                                helpers : internals.hbs.helpers
+                            };
 
 //
 // Notify when task has done
 // -----------------------------------------------------------------------
-	gulp.on('task_stop', function(e) {
+    gulp.on('task_stop', function(e) {
 
-		var quiet = (argv.quiet) ? true : false;
+    var quiet = (argv.quiet) ? true : false;
 
-		if (!quiet) {
+    if (!quiet) {
 
-			var time = prettyTime(e.hrDuration);
-			gulp.src('').pipe(notify({
-				title: "Finished: "+ e.task.toUpperCase(),
-				message: "after " + time
-			}));
-		}
-	});
+        var time = prettyTime(e.hrDuration);
+            gulp.src('').pipe(notify({
+                title: "Finished: "+ e.task.toUpperCase(),
+                message: "after " + time
+            }));
+        }
+    });
 
 //
 // Connect task
 // -----------------------------------------------------------------------
-	gulp.task('connect', () => {
+    gulp.task('connect', () => {
 
-		connect.server({
-			root: internals.paths.dist.root,
-			port: 8000,
-			livereload: true
-		});
-	});
+        connect.server({
+            root: internals.paths.dist.root,
+            port: 8000,
+            livereload: true
+        });
+    });
 
 
 //
 // Process templates
 // -----------------------------------------------------------------------
-	gulp.task('templates', () => {
+    gulp.task('templates', () => {
 
-		return gulp.src([internals.paths.dev.hbs.root+'/**/*.hbs', '!'+internals.paths.dev.hbs.partials+'/**/*.hbs'])
-				   .pipe(handlebars(internals.web, internals.hbs.options))
-				   .pipe(rename(function(path) {
-				   		path.extname = '.html';
-				   }))
-				   .pipe(gulp.dest(paths.dist.root))
-				   .pipe(connect.reload());
-	});
+        return gulp.src([internals.paths.dev.hbs.root+'/**/*.hbs', '!'+internals.paths.dev.hbs.partials+'/**/*.hbs'])
+               .pipe(handlebars(internals.web, internals.hbs.options))
+               .pipe(rename(function(path) {
+                   path.extname = '.html';
+               }))
+               .pipe(gulp.dest(paths.dist.root))
+               .pipe(connect.reload());
+    });
 
 //
 // Watch changes
@@ -91,4 +91,4 @@
 //
 // Default Task
 // -----------------------------------------------------------------------
-	gulp.task('default', ['connect', 'templates', 'watch']);
+    gulp.task('default', ['connect', 'templates', 'watch']);
