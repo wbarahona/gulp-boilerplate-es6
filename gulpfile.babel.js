7cd4b09288fdf268ae78d8977be9c2afd635ad66
Willmer Barahona <wbarahona@live.com>
2016-11-17 10:34:57 -0600

    now processing es6 lint, scripts, img, sass, fonts
    added linting for es6, scripts has babel to use es6 where unavailable,
    minify images, processing sass and copy pasting the fonts files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,12 +8,33 @@
     import notify from 'gulp-notify';
     import { argv } from 'yargs';
     import prettyTime from 'pretty-hrtime';
+    import sourcemaps from 'gulp-sourcemaps';
+    import gutil from 'gulp-util';
 
     // front end server related
     import connect from 'gulp-connect';
 
     // view/html/templating related
     import handlebars from 'gulp-compile-handlebars';
+
+    // scripts and lint related
+    import eslint from 'gulp-eslint';
+    import browserify from 'browserify';
+    import watchify from 'watchify';
+    import glob from 'glob';
+    import buffer from 'vinyl-buffer';
+    import source from 'vinyl-source-stream';
+    import uglify from 'gulp-uglify';
+    import babelify from 'babelify';
+
+    // Styles related
+    import sass from 'gulp-sass';
+    import cssnano from 'gulp-cssnano';
+    import combinemq from 'gulp-combine-mq';
+
+    // Image related
+    import imagemin from 'gulp-imagemin';
+    import pngquant from 'imagemin-pngquant';
 
     // application configurations
     import paths from './app/client/conf/paths.conf';
@@ -40,13 +61,13 @@
 //
 // Notify when task has done
 // -----------------------------------------------------------------------
-    gulp.on('task_stop', function(e) {
+    gulp.on('task_stop', (e) => {
 
-    var quiet = (argv.quiet) ? true : false;
+        var quiet = (argv.quiet) ? true : false;
 
-    if (!quiet) {
+        if (!quiet) {
 
-        var time = prettyTime(e.hrDuration);
+            var time = prettyTime(e.hrDuration);
             gulp.src('').pipe(notify({
                 title: "Finished: "+ e.task.toUpperCase(),
                 message: "after " + time
@@ -66,19 +87,144 @@
         });
     });
 
+//
+// Swallow Error
+// -----------------------------------------------------------------------
+    const swallowError = (error) => {
+        console.log(error.toString());
+        this.emit('end');
+    }
+
+//
+// Process scripts
+// -----------------------------------------------------------------------
+    const buildDir = (entry) => {
+
+        const newstring = entry.replace(internals.paths.dev.scripts,'');
+        const lastOccurrenceIndex = newstring.lastIndexOf("/");
+        const dir = newstring.slice(0,lastOccurrenceIndex+1);
+
+        return dir;
+    };
+
+    const bundleScript = (file) => {
+
+        const opts = Object.assign({}, watchify.args, {
+                        debug: true,
+                        entries: file
+                   });
+        const lint = (src) => {
+            return gulp.src(src)
+                       .pipe(eslint({ useEslintrc: true }))
+                       .pipe(eslint.format());
+        };
+        let brw = browserify(opts);
+        const rebundle = () => {
+
+            return brw.bundle()
+                      .on('error', gutil.log.bind(gutil, '>>> Bundling error!'))
+                      .pipe(source(file))
+                      .pipe(buffer())
+                      .pipe(uglify({options: {
+                          mangle: {
+                              except: ['jquery']
+                          }
+                      }}))
+                      .pipe(sourcemaps.init({ loadMaps: true }))
+                      .pipe(sourcemaps.write('./maps'))
+                      .pipe(rename({
+                          dirname: buildDir(file),
+                          extname: '.min.js'
+                      }))
+                      .pipe(gulp.dest(internals.paths.dist.scripts))
+                      .pipe(connect.reload());
+        };
+
+        brw.transform(babelify.configure({
+            global: true,
+            compact: false
+        }));
+        brw = watchify(brw);
+        brw.on('update', (id) => {
+            lint(id);
+            rebundle();
+        });
+
+        return rebundle();
+    };
+
+    gulp.task('scripts', () => {
+
+        glob(`${ internals.paths.dev.scripts }/**/*.js`,
+            {'ignore': [`${ internals.paths.dev.scripts }/modules/**/*`, `${ internals.paths.dev.scripts }/libs/**/*`]},
+            (err, files) => {
+
+                if(err) swallowError(err);
+                const tasks = files.map((entry, index) => {
+                    bundleScript(entry);
+                });
+        });
+    });
+
+//
+// Process Styles
+// -----------------------------------------------------------------------
+    gulp.task('sass', () => {
+
+        var sassOptions = {
+            errLogToConsole: true
+        };
+
+        return gulp.src([`${ internals.paths.dev.styles }/styles.scss`])
+                   .pipe(sass(sassOptions).on('error', sass.logError))
+                   .pipe(combinemq())
+                   .pipe(cssnano({
+                       autoprefixer: { browsers: ['last 3 version'], add: true }
+                   }))
+                   .pipe(sourcemaps.init())
+                   .pipe(sourcemaps.write())
+                   .pipe(rename('styles.min.css'))
+                   .pipe(gulp.dest(internals.paths.dist.styles))
+                   .pipe(connect.reload());
+    });
+
+//
+// Process images
+// -----------------------------------------------------------------------
+    gulp.task('images', () => {
+
+        return gulp.src(`${ internals.paths.dev.images }/**/*`)
+                   .pipe(imagemin({
+                       progressive: true,
+                       svgoPlugins: [{removeViewBox: false}],
+                       use: [pngquant()]
+                   }))
+                   .pipe(gulp.dest(internals.paths.dist.images))
+                   .pipe(connect.reload());
+    });
+
+//
+// Process font files
+// -----------------------------------------------------------------------
+    gulp.task('fonts', () => {
+
+        return gulp.src(`${ internals.paths.dev.fonts }/**/*`)
+                   .pipe(gulp.dest(internals.paths.dist.fonts))
+                   .pipe(connect.reload());
+    });
 
 //
 // Process templates
 // -----------------------------------------------------------------------
     gulp.task('templates', () => {
 
-        return gulp.src([internals.paths.dev.hbs.root+'/**/*.hbs', '!'+internals.paths.dev.hbs.partials+'/**/*.hbs'])
-               .pipe(handlebars(internals.web, hbs.options))
-               .pipe(rename(function(path) {
-                   path.extname = '.html';
-               }))
-               .pipe(gulp.dest(paths.dist.root))
-               .pipe(connect.reload());
+        return gulp.src([`${ internals.paths.dev.hbs.root }/**/*.hbs`, `!${ internals.paths.dev.hbs.partials }/**/*.hbs`])
+                   .pipe(handlebars(internals.web, hbs.options))
+                   .pipe(rename((path) => {
+                       path.extname = '.html';
+                   }))
+                   .pipe(gulp.dest(paths.dist.root))
+                   .pipe(connect.reload());
     });
 
 //
@@ -86,10 +232,12 @@
 // -----------------------------------------------------------------------
     gulp.task('watch', () => {
 
-        gulp.watch([internals.paths.dev.hbs.root+'/**/*.hbs'], ['templates']);
+        gulp.watch([`${ internals.paths.dev.hbs.root }/**/*.hbs`], ['templates']);
+        gulp.watch([`${ internals.paths.dev.styles }/**/*.scss`], ['sass']);
+        gulp.watch([`${ internals.paths.dev.images }/**/*`], ['images']);
     });
 
 //
 // Default Task
 // -----------------------------------------------------------------------
-    gulp.task('default', ['connect', 'templates', 'watch']);
+    gulp.task('default', ['connect', 'templates', 'sass', 'images', 'fonts', 'scripts', 'watch']);
